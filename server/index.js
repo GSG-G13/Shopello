d648bc46d3be549b57947b12a94ce4299f78dc55
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-05-21 16:20:03 +0300

    delete comment from index file

--- server/index.js
+++ server/index.js
@@ -1,83 +1,23 @@
-const express = require('express');
-const db = require('./database/config/connection');
-const { User, CartItems } = require('./database/models/index');
+const express = require("express");
+const db = require("./database/config/connection");
+const { User, CartItems } = require("./database/models/index");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app
-  .get('/Users', (req, res) => {
-    User.findAll()
-      .then((users) => {
-        res.json(users);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  })
-  .get('/CartItems', (req, res) => {
-    CartItems.findAll()
-      .then((cartItems) => {
-        res.json(cartItems);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-//   .post('/Users', (req, res) => {
-//     User.create(req.body)
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         res.json(err);
-//       });
-//   })
-//   .get('/Users/:id', (req, res) => {
-//     User.findByPk(req.params.id)
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         res.json(err);
-//       });
-//   })
-//   .put('/Users/:id', (req, res) => {
-//     User.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     })
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         res.json(err);
-//       });
-//   })
-//   .delete('/Users/:id', (req, res) => {
-//     User.destroy({
-//       where: {
-//         id: req.params.id,
-//       },
-//     })
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         res.json(err);
-//       });
-//   });
+/*
+  CURD operations are here
+*/
 
 db.authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
   .catch((err) => {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
   });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://127.0.0.1:3000');
+  console.log("Server is running on http://127.0.0.1:3000");
 });
