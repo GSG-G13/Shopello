75ff4c31cea22778b4d81425ec0b173dba108c90
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-05-22 12:10:15 +0300

    signin middleware, controller and some utils file

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import db from './database/config/connection';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*
+  CURD operations are here
+*/
+
+// app.post('/api/user/login', ()=>{});
+// app.post('/api/user/singup', ()=>{});
+
+db.authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database:', err);
+  });
+
+export default app;
---
+++ server/utils/validation/signupSchema.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+const signupSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.email': 'Email must be a valid email',
+    'string.empty': 'Email is required',
+    'any.required': 'Email is required',
+  }),
+  username: Joi.string().min(4).required().message({
+    'string.min': 'Username must be at least 4 characters long',
+    'string.empty': 'Username is required',
+    'any.required': 'Username is required',
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.min': 'Password must be at least 6 characters long',
+    'string.empty': 'Password is required',
+    'any.required': 'Password is required',
+  }),
+});
+
+export default signupSchema;
