4059688cb1b9db200b2cc35019612416ef29baeb
FarahAlhalimi <engfarahhalimy@gmail.com>
2023-05-22 12:37:53 +0300

    cart initial files

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Empty from './Empty';
+
+const CartContainer = styled.div`
+  margin-top: 20px;
+  position: absolute;
+width: 375px;
+height: 745px;
+left: 0px;
+top: 67px;
+background: #FFFFFF;
+border-radius: 20px 20px 0px 0px;
+`;
+
+const CartTitle = styled.h1`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const CartItem = styled.li`
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+background: #FFFFFF;
+box-shadow: 0px 8px 24px rgba(128, 128, 128, 0.15);
+border-radius: 20px; 
+margin-bottom: 10px;
+`;
+
+const CartItemTitle = styled.h3`
+left: 30.58%;
+right: 29.05%;
+top: 23%;
+bottom: 58%;
+font-family: 'Gilroy-Regular';
+font-size: 16px;
+line-height: 19px;
+color: #1D1D35;
+`;
+
+const CartItemPrice = styled.p`
+
+left: 79.82%;
+right: 7.34%;
+top: 23%;
+bottom: 57%;
+font-family: 'Gilroy-Bold';
+font-size: 16px;
+line-height: 19px;
+color: #1D1D35;
+`;
+
+const RemoveButton = styled.button`
+left: 81.35%;
+right: 7.34%;
+top: 69%;
+bottom: 19%;
+font-family: 'Gilroy-Regular';
+font-size: 10px;
+line-height: 12px;
+text-decoration-line: underline;
+color: #FA6161;
+`;
+
+function Cart() {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    fetch('/cart')
+      .then((response) => response.json())
+      .then((data) => setCartItems(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleRemoveFromCart = (itemId) => {
+    fetch(`/cart/${itemId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log('Item removed from cart');
+          fetch('/cart')
+            .then((response) => response.json())
+            .then((data) => setCartItems(data))
+            .catch((error) => console.error(error));
+        } else {
+          console.error('Failed to remove item from cart');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <CartContainer>
+      <CartTitle>Cart</CartTitle>
+      {cartItems.length === 0 ? (
+        <Empty />
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <CartItem key={item.id}>
+              <CartItemTitle>{item.name}</CartItemTitle>
+              <CartItemPrice>
+                Price:
+                {item.price}
+
+              </CartItemPrice>
+              <RemoveButton onClick={() => handleRemoveFromCart(item.id)}>Remove from Cart</RemoveButton>
+            </CartItem>
+          ))}
+        </ul>
+      )}
+    </CartContainer>
+  );
+}
+
+export default Cart;
---
+++ client/src/components/Cart/CartBtn.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 8px 16px;
+  border: none;
+  background: #2CB674;
+border-radius: 50px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+
+  &:hover {
+    background-color: #ccc;
+  }
+`;
+function AddToCart({ productId }) {
+  const handleAddToCart = () => {
+    fetch('/addToCart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ productId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log('Product added to cart');
+        } else {
+          console.error('Failed to add product to cart');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <Button onClick={handleAddToCart}>Add to Cart</Button>
+  );
+}
+
+export default AddToCart;
