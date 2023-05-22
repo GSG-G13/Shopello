c7fa1135647551787bd20ebe8e3c273b77f9a299
raghdarizq <raghdarizq2@gmail.com>
2023-05-22 11:41:09 +0300

    accountStyled

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import SingIn from './components/Account/SingIn';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <SingIn />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
+import SingIn from './components/Account/SingIn';
+import SingUp from './components/Account/SingUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/sining',
+    element: <SingIn />,
+  },
+  {
+    path: '/singup',
+    element: <SingUp />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
-
