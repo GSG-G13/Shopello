fe07308ca137b2c4ce11eaf0abea68bc9ed3420b
raghdarizq <raghdarizq2@gmail.com>
2023-05-24 09:46:09 +0300

    detalis

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -5,24 +5,27 @@ import {
 } from './DetailsStyled';
 
 const Details = () => (
-  <Wrapper>
-    <Img src="https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg" alt="" />
-    <NamePrice>
-      <span className="name">Electronic</span>
-      <span className="price">$55</span>
-    </NamePrice>
-    <h4>Lorem ipsum dolor sit amet consectetur.</h4>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero non earum quia!</p>
-    <div>
-      <span>
-        Total:
-        <span>$110</span>
-      </span>
-    </div>
-    <div className="button-group">
-      <Bth>Add to Cart</Bth>
-    </div>
-  </Wrapper>
+  <>
+    <i className="ri-corner-up-left-line" />
+    <Wrapper>
+      <Img src="https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg" alt="" />
+      <NamePrice>
+        <span className="name">Electronic</span>
+        <span className="price">$55</span>
+      </NamePrice>
+      <h4>Lorem ipsum dolor sit amet consectetur.</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero non earum quia!</p>
+      <div>
+        <span>
+          Total:
+          <span>$110</span>
+        </span>
+      </div>
+      <div className="button-group">
+        <Bth>Add to Cart</Bth>
+      </div>
+    </Wrapper>
+  </>
 );
 
 export default Details;
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import {
 import SingIn from './components/account/SingIn.jsx';
 import SingUp from './components/account/SingUp.jsx';
 import Home from './pages/home/Home.jsx';
+import Details from './components/details/Details.jsx';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: '/singup',
     element: <SingUp />,
+  },
+  {
+    path: '/d',
+    element: <Details />,
   },
 ]);
 
